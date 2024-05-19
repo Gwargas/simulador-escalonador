edff63dfcd958527665d9f0e4c094815c0605a81
David Dias <58055711+DavidDiasLugon@users.noreply.github.com>
2024-05-19 00:44:22 +0000

    small changes and addition of comments

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 class Memoria_RAM:
     capacidade = 34359738368  # 32Gbytes
+    lista_prontos = []
 
-    def adicionar_processo(self):
-        ...
+    def adicionar_processo(self, processo):
+        if (self.capacidade - processo.tam) < 0: # Caso não haja espaço na memória primária
+            ... 
     # Enviar processo para memória secundária
     def swapp_out(self):
         ...
@@ -25,11 +27,11 @@ class Processo:
         self.f1 = fase1
         self.io = entrada_saida 
         self.f2 = fase2
-        self.tam = tamanho  # Em Mbytes
+        self.tam = (tamanho * 1048576) # Em Mbytes
         self.d = disco
-    def mbytes_para_bytes(self):
-        return (self.tam * 1048576)
+
     def enviar_memoria_primaria(self):
+        
         ...
 
 class Processador:
@@ -50,7 +52,8 @@ class Computador:
     cpu3 = Processador('cpu3') 
     cpu4 = Processador('cpu4')
 
-    with open('input', 'r') as arq:
+    # Leitura do arquivo com cada um dos processos
+    with open('input', 'r') as arq:  
         for linha in arq:
             processo_linha = [x.split() for x in linha.split(',')]
             match linha[5]:
