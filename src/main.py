e4255285bbdb811a1d6df2e36e6bb5c683ad5e01
David Dias <58055711+DavidDiasLugon@users.noreply.github.com>
2024-06-06 01:03:25 +0000

    Meeting alteration

--- src/main.py
+++ src/main.py
@@ -1,24 +1,44 @@
 import queue
+import random
+
+random.seed()
+
+def gera_id():
+    palavra = ""
+    for i in range(50):
+        letra = random.randint(33, 127)
+        j = chr(letra)
+        palavra += j
+    return palavra
+
+class PCB:
+    def __init__(self, ident, p):
+        self.estadoProcesso = "Novo"
+        self.idProcesso = ident
+        self.prioridade = p
 
 class Processo:
     def __init__(self, chegada, fase1, entrada_saida, fase2, tamanho, disco):
         self.c = chegada
         self.f1 = fase1
-        self.io = entrada_saida 
+        self.io = entrada_saida
         self.f2 = fase2
-        self.tam = (tamanho * 1048576) # Em Mbytes
+        self.tam = (tamanho * 1048576)  # Em Mbytes
         self.d = disco
-        self.p = 1 # Define a prioridade do processo, caso seja bloqueado p += 1, e sempre começa com 1
+        self.p = 1  # Define a prioridade do processo, caso seja bloqueado p += 1, e sempre começa com 1
+        self.momento_saida = chegada + fase1 + entrada_saida + fase2  # TODO: Guardar quanto tempo o processo gasta em cada fase
+        self.pcb = PCB(gera_id(), self.p)
+
     def enviar_memoria_primaria(self):
         ...
 
 class Memoria_RAM:
     capacidade = 34359738368  # 32Gbytes
-    # Instanciando as quarto filas de processos prontos para serem executados 
-    filas = [queue.Queue() for _ in range(4)]
+    filas = [queue.Queue() for _ in range(4)]  # Instanciando as quatro filas de processos prontos para serem executados
+    bloqueados = []
 
     def adicionar_processo(self, processo: Processo):
-        if (self.capacidade - processo.tam) < 0: # Caso não haja espaço na memória primária
+        if (self.capacidade - processo.tam) < 0:  # Caso não haja espaço na memória primária
             return -1
         if (processo.p == 1):
             self.filas[0].put(processo)
@@ -29,42 +49,57 @@ class Memoria_RAM:
         elif (processo.p == 4):
             self.filas[3].put(processo)
 
-    # Enviar processo para memória secundária
-    def swapp_out(self):
-        ...
-
 class Memoria_secundaria:
+    bloqueados = []
+
     def __init__(self, identificador):
         self.id = identificador
-        self.armazenamento = [] # O armazenamento será mesmo infinito?
+        self.armazenamento = []  # O armazenamento será mesmo infinito?
 
     def adicionar_processo(self, processo: Processo):
         self.armazenamento.append(processo)
 
-    # Enviar processo para memória primária
-    def swapp_in(self):
-        ...
-
 class Processador:
     def __init__(self, identificador):
-        self.id = identificador 
+        self.id = identificador
+
+class Escalonador:
+    def __init__(self, identificador):
+        self.id = identificador
+
+class Despachante(Escalonador):  # Curto Prazo
+    def __init__(self, identificador):
+        super(Despachante, self).__init__(identificador)  # Herdando do escalonador
+
+    def Despacho(self):  # Seleciona processo para receber o estado de Executando
+        ...
+
+class MedioPrazo(Escalonador):
+    def __init__(self, identificador):
+        super(MedioPrazo, self).__init__(identificador)  # Herdando do escalonador
+
+    def SwapOut(self, processo, ram, secundaria):  # RAM -> Mem.Sec.
+        ...
+
+    def Swapping(self, processo, ram, secundaria):  # Mem.Sec -> RAM; é preciso tirar o processo da lista de processos que estão na Mem.Sec. e jogá-lo pra lista da RAM
+        ...
 
 class Computador:
     # Inicialização da memória principal
     ram = Memoria_RAM()
-    # Inicialização dos discos 
+    # Inicialização dos discos
     disco1 = Memoria_secundaria('disco1')
     disco2 = Memoria_secundaria('disco2')
     disco3 = Memoria_secundaria('disco3')
     disco4 = Memoria_secundaria('disco4')
-    # Inicialização dos processadores 
+    # Inicialização dos processadores
     cpu1 = Processador('cpu1')
     cpu2 = Processador('cpu2')
-    cpu3 = Processador('cpu3') 
+    cpu3 = Processador('cpu3')
     cpu4 = Processador('cpu4')
 
     # Leitura do arquivo com cada um dos processos
-    with open('input.txt', 'r') as arq:  
+    with open('input.txt', 'r') as arq:
         for linha in arq:
             processo_linha = [x.split() for x in linha.split(',')]
-            
+            # Further processing of processo_linha
